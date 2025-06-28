4cbc46e06c84d12477a071abc0e9a3b2c46c1a0b
Robin Linacre <robinlinacre@hotmail.com>
2025-06-28 18:46:14 +0100

    fix: Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/quick_soundboard/', // For GitHub Pages deployment
+  base: '/soundboard_launchpad_mini_mkii/', // For GitHub Pages deployment
   plugins: [
     react(),
     tailwindcss(),
